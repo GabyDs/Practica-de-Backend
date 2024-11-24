a954e42e96d14d3234e8d3bbce821af8ad570871
GabyDs <gabydsch2001@gmail.com>
2024-11-24 18:32:41 -0300

    feat: add was_published_recently method and admin integration for Question model
    - Added was_published_recently method to Question model:
      - Determines if the question was published within the last day.
    - Configured timezone for the project to match local settings.
    - Registered Question model in Django admin panel for management.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Each model has a number of class variables, each of which represents a database field in the model.
 
@@ -8,6 +11,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
