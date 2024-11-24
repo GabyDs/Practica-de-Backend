50e1347e3570bee78cd8cd71edf71703395be8c1
GabyDs <gabydsch2001@gmail.com>
2024-11-24 18:03:27 -0300

    feat: add Question and Choice models and register polls app
    - Defined Question model with fields:
      - question_text: stores the question text
      - pub_date: stores the publication date
    - Defined Choice model with fields:
      - question: foreign key linking to a Question
      - choice_text: stores the choice text
      - votes: integer to track votes for each choice
    - Registered polls app in settings.INSTALLED_APPS

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Each model has a number of class variables, each of which represents a database field in the model.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+# First we need to tell our project that the polls app is installed.
+# Edit the mysite/settings.py file and add that dotted path to the INSTALLED_APPS setting.
