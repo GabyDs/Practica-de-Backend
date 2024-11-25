5b1c868a59b539ae40e914c2000023c2e221c2fd
GabyDs <gabydsch2001@gmail.com>
2024-11-25 17:27:43 -0300

    feat: implement views and template for polls app functionality
    - Added views for the following functionalities:
      - `index`: Displays the latest 5 questions using the `render` function and template.
      - `detail`: Displays a placeholder text for the detail view of a question.
      - `results`: Displays a placeholder text for viewing results of a specific question.
      - `vote`: Displays a placeholder text for voting on a specific question.
    - Updated the `index` view to:
      - Fetch the latest 5 questions from the database.
      - Pass the list to the `polls/index.html` template.
    - Created the `polls/index.html` template:
      - Lists the latest questions as links.
      - Displays a message if no questions are available.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,40 @@
 from django.http import HttpResponse
 
+# from django.template import loader
 
+from django.shortcuts import render
+
+from .models import Question
+
+# Muestra las ultimas 5 preguntas de la encuesta
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    
+    # template = loader.get_template("polls/index.html")
+    
+    context = {
+        "latest_question_list": latest_question_list,
+    }
 
+    # La función render() toma el objeto de la solicitud como su primer argumento,
+    # un nombre de plantilla como su segundo argumento y un diccionario como su tercer argumento opcional.
+    # La función devuelve un objeto HttpResponse de la plantilla que se ha proporcionado representada con el contexto dado
+    return render(request, "polls/index.html", context)
+
+    # return HttpResponse(template.render(context, request))
+
+    # output = ", ".join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+# muestra un texto de pregunta sin resultados, pero con un formulario para votar.
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+# muestra los resultados de una pregunta en particular.
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+# gestiona la votación para una elección concreta en una pregunta específica.
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
