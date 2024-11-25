cd947293b356e69f8e94ded5fb3dfa0c00f47694
GabyDs <gabydsch2001@gmail.com>
2024-11-25 17:55:52 -0300

    feat: enhance detail view and add namespace for polls app
    - Updated the `detail` view:
      - Now uses `get_object_or_404` to handle invalid question IDs gracefully by returning a 404 error.
      - Renders the `polls/detail.html` template with the selected question.
    - Added `polls/detail.html` template:
      - Displays the question text.
      - Lists all associated choices for the question.
    - Modified `urls.py`:
      - Added `app_name = "polls"` to define a namespace for the app.
      - Updated routes to support named URLs (e.g., `detail`, `results`, `vote`).
      - Ensures clear resolution of routes even with similar names across different apps.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
-# from django.template import loader
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 # Muestra las ultimas 5 preguntas de la encuesta
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    
-    # template = loader.get_template("polls/index.html")
-    
+        
     context = {
         "latest_question_list": latest_question_list,
     }
@@ -21,14 +17,19 @@ def index(request):
     # La función devuelve un objeto HttpResponse de la plantilla que se ha proporcionado representada con el contexto dado
     return render(request, "polls/index.html", context)
 
-    # return HttpResponse(template.render(context, request))
-
-    # output = ", ".join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-
 # muestra un texto de pregunta sin resultados, pero con un formulario para votar.
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+
+    # try:
+    #     # Si existe un question con determinado id
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    # Lo mismo que el try anterior
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/detail.html", {"question": question})
 
 # muestra los resultados de una pregunta en particular.
 def results(request, question_id):
