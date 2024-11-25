4ad449f10cde5cbf57eb81e0fdf9f346a954caf0
GabyDs <gabydsch2001@gmail.com>
2024-11-25 20:21:09 -0300

    chore: remove two_endpoints from repository

--- mysite/two_endpoints/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TwoEndpointsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'two_endpoints'
--- mysite/two_endpoints/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("user/", views.user, name="user"),
-    path("admin/", views.admin, name="admin")
-]
